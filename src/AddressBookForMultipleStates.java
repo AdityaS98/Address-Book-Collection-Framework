2d7212287b119e7a0e97b58b0ad32517dfd92c1b
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-17 14:00:13 +0530

    Adding a program for searching contacts in address book across multiple states

---
+++ src/AddressBookForMultipleStates.java
@@ -0,0 +1,103 @@
+package com.CollectionFramework;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * 
+ * @author DELL
+ *
+ */
+
+
+public class AddressBookForMultipleStates {
+	/**
+	 * 
+	 * @param args
+	 */
+
+public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in); 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+		
+		/*
+		 * To create address book with city and person information
+		 */
+		HashMap<String,Contact> addressBook1=new HashMap<String,Contact>();
+		
+		/*
+		 * To create address book with state and person inforamtion
+		 */
+		HashMap<String,Contact> addressBook2=new HashMap<String,Contact>();
+		
+		/*
+		 * To take user input for pesron information we are making person object with different information
+		 * 1)First name 2)Second name 3) Address 4)City 5)State 6)Zip code 7)Phone number 8)Email
+		 */
+		System.out.println("enter person1 information");
+		Contact p1 = new Contact(br.readLine(),br.readLine(),br.readLine(),br.readLine(),br.readLine(), br.read(),br.readLine(),br2.readLine());
+		System.out.println("enter person2 information");
+		Contact p2 = new Contact(br1.readLine(),br1.readLine(),br1.readLine(),br1.readLine(),br1.readLine(), br1.read(),br1.readLine(),br1.readLine());
+		System.out.println("enter person3 information");
+		Contact p3 = new Contact(br2.readLine(),br2.readLine(),br2.readLine(),br2.readLine(),br2.readLine(), br2.read(),br2.readLine(),br2.readLine());
+	
+		/*
+		 * Adding to address book with respect to city 
+		 */
+		
+		addressBook1.put(p1.getCity(), p1);
+		addressBook1.put(p2.getCity(), p2);
+		addressBook1.put(p3.getCity(), p3);
+		
+		System.out.println("Enter city to search");
+		String city = sc.next();
+		
+		/*
+		 * To display our addressbook1
+		 * 
+		 */
+		Set<Map.Entry<String,Contact>> entries = addressBook1.entrySet();
+		
+		int count = 0;
+		for(Map.Entry<String,Contact> p : entries) {
+			if(p.getKey()==city) {
+				count++;
+				Contact person = p.getValue();
+				System.out.println(person);
+			}
+		}
+		System.out.println(count+"are numbers of persons in address book");
+		
+		/*
+		 * Adding to address book with respect to state
+		 */
+		
+		addressBook2.put(p1.getState(), p1);
+		addressBook2.put(p2.getState(), p2);
+		addressBook2.put(p3.getState(), p3);
+		
+		
+		System.out.println("Enter state to search");
+		String state = sc.next();
+		int count1=0;
+		
+		/*
+		 * To count persons from addressbook2
+		 * 
+		 */
+		Set<Map.Entry<String,Contact>> entries2 = addressBook2.entrySet();
+		
+		for(Map.Entry<String,Contact> p : entries2) {
+			if(p.getKey()==state) {
+				count1++;
+			Contact person = p.getValue();
+			System.out.println(person);
+		}
+		}
+		System.out.println(count1+"are numbers of persons in address book");
+	}
+}
